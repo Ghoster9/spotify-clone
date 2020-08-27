e21c8bbf5e6c218e870c3b9701429707e8de35ea
ahhzaky <siahmadzaky@gmail.com>
2020-08-27 22:41:46 +0700

    Access data api in console,log

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Login from './Login/Login';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login/Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
-  
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("🧑", user);
+      });
     }
 
     console.log("I Have a token: ", token);
   }, []);
 
   return (
-    <div className="App">
-      {
-        token ? (
-          <h1>i am logged in</h1>
-        ) : (
-          <Login />
-        )
-      }
-    </div>
+    <div className="App">{token ? <h1>i am logged in</h1> : <Login />}</div>
   );
 }
 
