58b19e994daabeb56ae9f355d3625211b4addb1f
ahhzaky <siahmadzaky@gmail.com>
2020-08-27 22:11:24 +0700

    add token and install spotify-web-api-js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login/Login';
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+  
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("I Have a token: ", token);
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <h1>i am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
