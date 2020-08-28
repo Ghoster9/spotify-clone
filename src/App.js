d87a11cc1f67156a3a2d0442db371c4ba40ce9f4
ahhzaky <siahmadzaky@gmail.com>
2020-08-28 12:23:33 +0700

    add file reducer, datalayer, update app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Login from "./Login/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from "./Player/Player"
+import Player from "./Player/Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -18,15 +20,21 @@ function App() {
     if (_token) {
       setToken(_token);
 
+
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log("🧑", user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
 
     console.log("I Have a token: ", token);
   }, []);
+
+  console.log("69", user);
 
   return (
     <div className="App">{token ?
