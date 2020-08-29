722efb2cee4a1c24f05fec06e8537f99efea3e26
ahhzaky <siahmadzaky@gmail.com>
2020-08-29 15:13:02 +0700

    get token

--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,11 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
-}
+    item: null,
+    //remove after finished developing
+    token:
+    "BQDdT1XtJWb9c6Y1y8ku5WU_JV1sFbrEshKRkOD00ZUK6x-f8cznG8IG7Oftg_cjIaPHDqMeibxLf_TgaHN_gYEFnynd0U1pKjrF-KMkfsS9-haIdVGZX-A1UbuA6z7c4ceVjZEbCiXONNnN5J7GcCEp-PDsZI1Pv65QEyR8gUgSPi1w",
+};
 
 const reducer = (state, action) => {
 console.log(action);
@@ -16,6 +19,12 @@ switch (action.type) {
             ...state,
             user:action.user
         }
+
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
         default:
             return state;
 }
